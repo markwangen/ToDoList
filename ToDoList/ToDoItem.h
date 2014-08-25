91e1bee333f91271cf473a48e87f55528e3e0528
mark.wang <mark.wang.en@gmail.com>
2014-08-26 02:08:11 +0800

    finish the second phase

---
+++ ToDoList/ToDoItem.h
@@ -0,0 +1,13 @@
+//
+//  ToDoItem.h
+//  ToDoList
+//
+//  Created by wang mark on 14-8-26.
+//  Copyright (c) 2014年 wang mark. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ToDoItem : NSObject
+
+@end
---
+++ ToDoList/ToDoItem.m
@@ -0,0 +1,13 @@
+//
+//  ToDoItem.m
+//  ToDoList
+//
+//  Created by wang mark on 14-8-26.
+//  Copyright (c) 2014年 wang mark. All rights reserved.
+//
+
+#import "ToDoItem.h"
+
+@implementation ToDoItem
+
+@end
